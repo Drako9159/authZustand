9503620cbf910b3d97c7ceaaa444d4c44fd27453
drako9159 <drako9159@gmail.com>
2023-01-08 00:50:13 -0600

    conigure tsc

---
+++ server/controllers/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export function loginHandler(req: Request, res: Response) {
+  //req.body = { email: "asdd@gmail.com" }
+  //validation, express-validation
+  // save to database
+  //create token ljhslduf98sydlkfhsiug68sdf9ygilfdg987
+  const token = jwt.sign(
+    {
+      email: "drakolin@gmail.com",
+    },
+    "secret",
+    {
+      expiresIn: 60 * 60 * 24, //24hrs
+    }
+  );
+  return res.json({ token: token });
+}
+export function profileHandler(req: Request, res: Response) {
+  return res.json({ message: "Hola perrin" });
+}
---
+++ server/routes/auth.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { loginHandler, profileHandler } from "../controllers/auth.controller";
+import { requireAuth } from "../middleware/requireAuth";
+const router = Router();
+
+router.post("/login", loginHandler);
+router.get("/profile", requireAuth, profileHandler);
+
+export default router;
